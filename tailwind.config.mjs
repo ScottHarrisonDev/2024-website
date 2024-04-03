90de1a73a609d605c438e3e0e83cc7df7be8520f
Scott Harrison <github@scottharrisondesign.co.uk>
2024-04-03 23:32:19 +0100

    add astro, mdx, tailwind. add new page stubs

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+		  'dark-grey': '#262730',
+		  'blue': '#c9cfff',
+		  'accent': '#5c6fff'
+		},
+		extend: {},
+	},
+	plugins: [],
+}
